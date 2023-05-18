715db6350ff2e5728a1852ed64db4d793d051b27
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-18 13:59:52 -0500

    add signup component

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { ProductsIndex } from "./ProductsIndex";
 import { ProductsNew } from "./ProductsNew";
+import { Signup } from "./Signup";
 
 export function Content() {
   const [products, setProducts] = useState([]);
@@ -28,6 +29,7 @@ export function Content() {
 
   return (
     <div>
+      <Signup />
       <ProductsNew onCreateProduct={handleCreateProduct} />
       <ProductsIndex products={products} />
     </div>
