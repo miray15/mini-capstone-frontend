44b80d8bda8d1988ca57cce37ae901a5f25dfc27
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-18 12:47:51 -0500

    create a new const function for handleCreateProduct on Content page and pass it to the new component

--- src/Content.jsx
+++ src/Content.jsx
@@ -14,11 +14,21 @@ export function Content() {
     });
   };
 
+  const handleCreateProduct = (params, successCallback) => {
+    console.log("handleCreateProduct", params);
+    axios
+      .post("http://localhost:3000/products.json", params)
+      .then((response) => {
+        setProducts([...products, response.data]);
+        successCallback();
+      });
+  };
+
   useEffect(handleIndexProducts, []);
 
   return (
     <div>
-      <ProductsNew />
+      <ProductsNew onCreateProduct={handleCreateProduct} />
       <ProductsIndex products={products} />
     </div>
   );
