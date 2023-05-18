ea36ed8176d7522399d581da3e08da7b832cc0ab
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-18 11:33:58 -0500

    add props function and map loop to ProductsIndex page

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,7 +1,14 @@
-export function ProductsIndex() {
+export function ProductsIndex(props) {
   return (
     <div>
-      <h1>All Products</h1>
+      <h1>All products</h1>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Price: {product.price}</p>
+          <p>Description: {product.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
