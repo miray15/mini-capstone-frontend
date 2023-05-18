2d5cf9644e2b58f141b50019d5de8de82fe83382
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-18 11:23:06 -0500

    pass placeholder data into index component as props

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,25 @@
 import { ProductsIndex } from "./ProductsIndex";
 
 export function Content() {
+  const products = [
+    {
+      id: 1,
+      name: "First",
+      url: "https://via.placeholder.com/150",
+      width: 150,
+      height: 150,
+    },
+    {
+      id: 2,
+      name: "Second",
+      url: "https://via.placeholder.com/300",
+      width: 300,
+      height: 300,
+    },
+  ];
   return (
     <div>
-      <ProductsIndex />
+      <ProductsIndex products={products} />
     </div>
   );
 }
