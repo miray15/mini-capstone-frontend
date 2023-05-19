5e73d80e2c903934b70d3fb32d5a012d0dcfa4b5
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-18 19:52:49 -0500

    add update action

--- src/Content.jsx
+++ src/Content.jsx
@@ -37,6 +37,25 @@ export function Content() {
     setCurrentProduct(product);
   };
 
+  const handleUpdateProduct = (id, params, successCallback) => {
+    console.log("handleUpdateProduct", params);
+    axios
+      .patch(`http://localhost:3000/products/${id}.json`, params)
+      .then((response) => {
+        setProducts(
+          products.map((product) => {
+            if (product.id === response.data.id) {
+              return response.data;
+            } else {
+              return product;
+            }
+          })
+        );
+        successCallback();
+        handleClose();
+      });
+  };
+
   const handleClose = () => {
     console.log("handleClose");
     setIsProductsShowVisible(false);
@@ -53,7 +72,10 @@ export function Content() {
       <ProductsNew onCreateProduct={handleCreateProduct} />
       <ProductsIndex products={products} onShowProduct={handleShowProduct} />
       <Modal show={isProductsShowVisible} onClose={handleClose}>
-        <ProductsShow product={currentProduct} />
+        <ProductsShow
+          product={currentProduct}
+          onUpdateProduct={handleUpdateProduct}
+        />
       </Modal>
     </div>
   );
--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -1,10 +1,35 @@
 export function ProductsShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onUpdateProduct(props.product.id, params, () => event.target.reset());
+  };
+
   return (
     <div>
       <h1>Product information</h1>
       <p>Name: {props.product.name}</p>
       <p>Price: {props.product.price}</p>
       <p>Description: {props.product.description}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name:
+          <input defaultValue={props.product.name} name="name" type="text" />
+        </div>
+        <div>
+          Price:
+          <input defaultValue={props.product.price} price="url" type="text" />
+        </div>
+        <div>
+          Description:
+          <input
+            defaultValue={props.product.description}
+            name="description"
+            type="text"
+          />
+        </div>
+        <button type="submit">Update product</button>
+      </form>
     </div>
   );
 }
